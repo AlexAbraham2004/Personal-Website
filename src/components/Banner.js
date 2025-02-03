ef2a12ada2d922ca8a5257b8242fc3eb5672cb1e
Alex Abraham <alexab6104@gmail.com>
2025-02-03 13:34:39 -0500

    Replaced chimp with threeJS rotating globe

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,8 @@ import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import MyGlobeComponent from "./global";
+
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -72,12 +74,9 @@ export const Banner = () => {
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
-            </TrackVisibility>
+              <div className="globe-container">
+                  <MyGlobeComponent />
+              </div>
           </Col>
         </Row>
       </Container>
