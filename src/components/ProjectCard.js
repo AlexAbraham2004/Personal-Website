0d1bfebaa5200064b7a520fd94d1492e655a2f76
Alex Abraham <alexab6104@gmail.com>
2026-01-03 23:22:08 -0500

    UI edits for smoother user experience

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,7 @@
 import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl, link }) => {
+  const isTrainTrip = title === "TrainTrip";
   return (
     <Col size={12} sm={6} md={4} className="project-card-container">
       <a 
@@ -9,7 +10,7 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
         rel="noopener noreferrer" 
         className="project-card-link"
       >
-        <div className="proj-imgbx">
+        <div className={`proj-imgbx ${isTrainTrip ? 'train-trip-card' : ''}`}>
           <img src={imgUrl} alt={title} />
           <div className="proj-txtx">
             <h4 style={{color:"white"}}>{title}</h4>
